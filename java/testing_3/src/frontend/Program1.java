44f388b240e0aa666c19894ada11d8d7997cd5c6
hoangthivui <hoangthivuit107@gmail.com>
2021-03-26 17:00:05 +0700

    test3

---
+++ java/testing_3/src/frontend/Program1.java
@@ -0,0 +1,12 @@
+package frontend;
+
+import backend.Question1Encapsulation;
+
+public class Program1 {
+	public static void main(String[] args) {
+		//Exercise1Contructor exercise1 = new Exercise1Contructor();
+		
+		Question1Encapsulation question1 = new Question1Encapsulation();
+		question1.quesStudent();
+	}
+}
